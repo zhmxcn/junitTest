82501bc23bc0f25d87b3da654b96f7edf72acc2c
zhmxcn <36781366+zhmxcn@users.noreply.github.com>
2018-08-21 16:36:48 +0800

    Update ParkingTest.java

--- Junit_Test/src/ParkingTest.java
+++ Junit_Test/src/ParkingTest.java
@@ -7,24 +7,34 @@ public class ParkingTest {
 	private Parking p = null; 
 	@Before
 	public void setUp() throws Exception {
-		p = new Parking();
-		p.setMax(1);
+		
 	}
 
 	@Test
 	public void when_park_car1_return_true() {
+		p = new Parking();
+		p.setMax(1);
 		assertEquals(true,p.park(new Car(1)));
 	}
 	@Test
 	public void when_park_car2_return_false() {
+		p = new Parking();
+		p.setMax(1);
+		p.park(new Car(1));
 		assertEquals(false,p.park(new Car(2)));
 	}
 	@Test
 	public void when_get_car1_return_true() {
+		p = new Parking();
+		p.setMax(1);
+		p.park(new Car(1));
 		assertEquals(true,p.getCar(1));
 	}
 	@Test
 	public void when_get_car1_return_false() {
+		p = new Parking();
+		p.setMax(1);
+		p.park(new Car(1));
 		assertEquals(false,p.getCar(1));
 	}
 	
